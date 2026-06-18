e577e7be848f1af64b35e7c2b28fe93a20e6a8dc
DeltaKhalid <khalid.delta063@gmail.com>
2026-06-18 14:53:03 +0600

    status bar color error has been solved

--- lib/application/app.dart
+++ lib/application/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../presentation/ui/screens/splash_screen.dart';
+import '../presentation/ui/utils/app_colors.dart';
 
 class DeltaLPGSalesApp extends StatelessWidget {
   const DeltaLPGSalesApp({super.key});
@@ -9,14 +11,15 @@ class DeltaLPGSalesApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Delta LPG Sales App",
-      // theme: ThemeData(
-      //   primaryColor: Colors.red, // 🔴 Change this to any color you want
-      //   colorScheme: ColorScheme.fromSwatch().copyWith(
-      //     primary: Colors.red, // Make sure primary color is applied consistently
-      //   ),
-      //   // Optional: You can also customize other colors like accent, background, etc.
-      //   scaffoldBackgroundColor: Colors.white,
-      // ),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarColor: AppColors.primaryButtonColor,
+            statusBarIconBrightness: Brightness.light, // white icons on green bg
+            statusBarBrightness: Brightness.dark,      // iOS equivalent
+          ),
+        ),
+      ),
       home: const SplashScreen(),
     );
   }
